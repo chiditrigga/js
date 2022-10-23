58f7b74b188637c5e5338c6617df62d4f2a53650
chiditrigga <105122478+chiditrigga@users.noreply.github.com>
2022-10-23 23:57:55 +0100

    final push

--- java.js
+++ java.js
@@ -1,4 +1,30 @@
-alert('whats your name?')
+let userName = window.prompt('enter username: ')
+let increase = 2;
+let number = Math.floor((Math.random() * increase) + 1);
+console.log(number)
+let guess = window.prompt(`${userName} select a number from 1-2`);
+let win = true;
 
+let stage = 1;
 
+let game = () => {
+    while (win === true){
+    if (guess == number){
+          alert(`${ userName} you win`);
+        increase++
+        stage++
+        number = Math.floor((Math.random() * increase) + 1);
+       console.log(number)
+       guess = window.prompt(`stage(${stage}) ${userName} select a number from 1-${increase}`)
 
+        win = true
+    }else{
+        
+        alert(`you lose at stage(${stage})`)
+        break;
+    }
+
+}
+}
+
+game();
